30d5d954a488d36975f1d72405eb7c41e8f4922d
Jesse Aldridge <JesseAldridge@gmail.com>
2016-09-14 21:08:52 -0700

    handle exceptions better

--- db_counter.py
+++ db_counter.py
@@ -1,4 +1,4 @@
-import os, shutil, time
+import os, shutil, time, traceback
 from datetime import datetime
 
 import records
@@ -6,35 +6,44 @@ import records
 import secrets
 
 
+def write_rotate(path, line):
+  with open(path, 'a') as f:
+   f.write(line)
+
+  # limit files to 1 MB
+  if os.path.getsize(path) > 1024 ** 2:
+    shutil.move(path, path + '.old')
+
 if not os.path.exists('data'):
   os.mkdir('data')
 
 while True:
-  for app_name, db_url_with_creds in secrets.app_db_tuples:
-    print 'app_name:', app_name
-    db = records.Database(db_url_with_creds)
+  try:
+    for app_name, db_url_with_creds in secrets.app_db_tuples:
+      print 'app_name:', app_name
+      db = records.Database(db_url_with_creds)
 
-    if not os.path.exists(os.path.join('data', app_name)):
-      os.mkdir(os.path.join('data', app_name))
+      if not os.path.exists(os.path.join('data', app_name)):
+        os.mkdir(os.path.join('data', app_name))
 
-    query = 'SELECT * FROM pg_catalog.pg_tables'
-    table_rows = db.query(query)
+      query = 'SELECT * FROM pg_catalog.pg_tables'
+      table_rows = db.query(query)
 
-    for table_row in table_rows:
-      table_name = table_row['tablename']
-      if table_name.startswith('pg_') and table_name != 'pg_locks':
-        continue
-      if table_name.startswith('sql_'):
-        continue
+      for table_row in table_rows:
+        table_name = table_row['tablename']
+        if table_name.startswith('pg_') and table_name != 'pg_locks':
+          continue
+        if table_name.startswith('sql_'):
+          continue
 
-      print '  counting table rows: {}'.format(table_name)
-      query = 'SELECT count(*) FROM {}'.format(table_name)
-      rows = db.query(query)
+        print '  counting table rows: {}'.format(table_name)
+        query = 'SELECT count(*) FROM {}'.format(table_name)
+        rows = db.query(query)
 
-      out_path = os.path.join('data', app_name, '{}.txt'.format(table_name))
-      with open(out_path, 'a') as f:
-       f.write('{} {}\n'.format(datetime.utcnow(), rows[0]['count']))
-
-      # limit files to 1 MB
-      if os.path.getsize(out_path) > 1024 ** 2:
-        shutil.move(out_path, out_path + '.old')
+        out_path = os.path.join('data', app_name, '{}.txt'.format(table_name))
+        write_rotate(out_path, '{} {}\n'.format(datetime.utcnow(), rows[0]['count']))
+  except Exception as e:
+    line = u'exception: {}; {}'.format(type(e).__name__, e.message).encode('utf8')
+    write_rotate('errors.txt', line)
+    write_rotate('errors.txt', traceback.format_exc())
+    time.sleep(10)
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync --exclude=".git" --exclude="junk" --exclude="data" -v -r . ubuntu@db_counter:~
+rsync --exclude=".git" --exclude="junk" --exclude="data" -v -r . ubuntu@db-counter:~
