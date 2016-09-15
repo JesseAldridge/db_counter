3f88e89a5ea1b7b98e27a4f57d463a132bc80e71
Jesse Aldridge <JesseAldridge@gmail.com>
2016-09-14 23:01:45 -0700

    break graphs onto multiple pages

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import sys, glob, os, json
+import sys, glob, os, json, re
 
 import flask
 
@@ -11,25 +11,33 @@ app.jinja_env.variable_end_string='}]}'
 
 @app.route('/')
 def index():
-  render_list = []
+  lines = []
   for app_name in os.listdir('data'):
     if not os.path.isdir(os.path.join('data', app_name)):
       continue
-    render_list.append({'html': '<h2>{}</h2>'.format(app_name)})
-    for path in glob.glob(os.path.join('data', app_name, '*.txt')):
-      table_name = os.path.basename(path.rsplit('.', 1)[0])
-      render_list.append({'html': '<h3>{}</h3>'.format(table_name)})
+    lines.append('<p><a href="/render_graphs/{}">{}</a></p>'.format(app_name, app_name))
+  return '\n'.join([line for line in lines])
 
-      with open(path) as f:
-        text = f.read()
-      dt_str_count_pairs =[]
-      for line in text.splitlines():
-        dt_str, count = line.rsplit(' ', 1)
-        count = int(count)
-        dt_str_count_pairs.append([dt_str, count])
-      render_list.append({'points': [dt_str_count_pairs]})
 
-  return flask.render_template('index.html', render_list=render_list)
+@app.route('/render_graphs/<app_name>')
+def render_graphs(app_name):
+  if not re.match('^[a-zA-Z0-9\-_]+$', app_name):
+    return 'bad app name', 400
+
+  render_list = [{'html': '<h2>{}</h2>'.format(app_name)}]
+  for path in glob.glob(os.path.join('data', app_name, '*.txt')):
+    table_name = os.path.basename(path.rsplit('.', 1)[0])
+    render_list.append({'html': '<h3>{}</h3>'.format(table_name)})
+
+    with open(path) as f:
+      text = f.read()
+    dt_str_count_pairs =[]
+    for line in text.splitlines():
+      dt_str, count = line.rsplit(' ', 1)
+      count = int(count)
+      dt_str_count_pairs.append([dt_str, count])
+    render_list.append({'points': [dt_str_count_pairs]})
+  return flask.render_template('render_graphs.html', render_list=render_list)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=port, debug=(port != 80))
