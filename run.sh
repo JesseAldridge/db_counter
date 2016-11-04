8cdb10b2ad303cd21373e4e662116221f9e37932
Jesse Aldridge <JesseAldridge@gmail.com>
2016-11-04 15:56:52 -0700

    pipe gunicorn to file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo killall gunicorn
-python db_counter.py 2>&1 | python logrot/logrot.py &
-sudo gunicorn -w 4 server:app -b 0.0.0.0:80 --log-file=- &
+python db_counter.py 2>&1 | python logrot/logrot.py db_counter_out.txt &
+sudo gunicorn -w 4 server:app -b 0.0.0.0:80 --log-file=- | python logrot/logrot.py gunicorn_out.txt &
